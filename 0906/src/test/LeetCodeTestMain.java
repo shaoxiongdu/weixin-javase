3d878dafe710c83976433d07d2fbcca8f582800b
shaoxiongdu <email@shaoxiongdu.cn>
2021-09-06 11:52:55 +0800

    0906 日常提交

---
+++ 0906/src/test/LeetCodeTestMain.java
@@ -0,0 +1,39 @@
+package test;
+
+/**
+ * @author: 杜少雄 <github.com/shaoxiongdu>
+ * @date: 2021年09月06日 | 8:18
+ * @description:
+ */
+public class LeetCodeTestMain {
+
+    public static int search(int[] nums, int target) {
+
+        if (nums.length < 0) return -1;
+
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+
+            //中点
+            int mind = (start + end) / 2;
+
+            if (nums[mind] == target) {
+                return mind;
+            } else if (nums[mind] > target) {
+                end = mind - 1;
+            } else if (nums[mind] < target) {
+                start = mind + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(search(new int[]{1, 2, 3, 4, 5, 6, 7}, 12));
+
+    }
+
+
+}
---
+++ 0906/src/test/TestMain.java
@@ -0,0 +1,21 @@
+package test;
+
+/**
+ * @author: 杜少雄 <github.com/shaoxiongdu>
+ * @date: 2021年09月06日 | 10:03
+ * @description:
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+
+        String s1 = null;
+
+        System.out.println(s1);
+    }
+
+    private static void swap(String s1) {
+        s1 = "2";
+    }
+
+}
